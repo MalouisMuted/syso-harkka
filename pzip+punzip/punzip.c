947960eb8bbc95edc0720f89cddd8276e1034988
Aleksi <aleksi.kuznetsov@gamil.com>
2018-12-11 13:20:36 +0200

    it works now

--- pzip+punzip/punzip.c
+++ pzip+punzip/punzip.c
@@ -5,81 +5,45 @@
 #include <ctype.h>
 #include <assert.h>
 #include <limits.h>
+#include <stdint.h>
 
-void uncompress_file(FILE *fp_in, FILE *fp_out);
+void uncompress_file(FILE *fp_in);
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
-		printf("Not enough arguments.\n");
+		fprintf(stderr, "Not enough arguments.\n");
 		exit(1);
 	}
 
-	FILE *fptr_w; 
-	char file_r[100], file_w[100];
-	// Last argv is target file
-	strcpy(file_w, argv[argc - 1]);
+	FILE *fptr_r; 
 
-	fptr_w = fopen(file_w, "w"); 
-	if (fptr_w == NULL) { 
-		printf("Cannot open file %s \n", file_w); 
-		exit(1); 
-	}
-
-	for (int i = 1; i < argc - 1; i++) {
-		// Open file for reading
-		FILE *fptr_r;
-		strcpy(file_r, argv[i]);
-		fptr_r = fopen(file_r, "rb"); 
+	for (int i = 1; i < argc; i++) {
+		fptr_r = fopen(argv[i], "rb"); 
 		if (fptr_r == NULL) { 
-			printf("Cannot open file %s \n", file_r); 
+			fprintf(stderr, "Cannot open file %s \n", argv[i]); 
 			exit(1); 
 		} else {
-			printf("uncompressing file %s\n", file_r);
-			uncompress_file(fptr_r, fptr_w);
+			uncompress_file(fptr_r);
 			fclose(fptr_r); 
 		}
 	}
-
-	fclose(fptr_w); 
 	return 0;
 }
 
-void uncompress_file(FILE *fp_in, FILE *fp_out) {
-	/*
-	int count, ch, ch2;
-
-	for (count = 0; ch2 != EOF; count = 0) {
-		ch = getc(fp_in);   // grab first byte
-		ch2 = getc(fp_in);  // grab second byte
-		// write the bytes
-		do {
-			putc(ch2, fp_out);
-			count++;
-		} while (count < ch);
-	}
-	*/
-	/*
-
-	unsigned int count;
-	int ch, ch2;
-	while (fp_in != EOF) {
-		for (count = 0; ch2 == ch && count < UINT_MAX; count++) {
-				ch2 = getc(fp_in);
-			}
-		fwrite(&count, 1, 4, fp_out);
-		fwrite(&ch, 1, 1, fp_out);
-		ch = ch2;
-	}
-	*/
-	unsigned int count;
+void uncompress_file(FILE *fp_in) {
+	uint32_t count;
 	int ch;
 
-	while (fread(stdout, 1, 5, fp_in) != EOF) {
-		count = fread(&count, 1, 4, fp_in);
-		ch = fread(&ch, 1, 1, fp_in);
+	while (1) {
+		fread(&count, 1, 4, fp_in);
+		fread(&ch, 1, 1, fp_in);
+
+		if (feof(fp_in)) { 
+         	break;
+      	}
 
 		for (int i = 0; i < count; i++) {
-			putc(ch, fp_out);
+			putc(ch, stdout);
 		}
 	}
 }
--- pzip+punzip/pzip.c
+++ pzip+punzip/pzip.c
@@ -6,45 +6,30 @@
 #include <assert.h>
 #include <limits.h>
 
-void compress_file(FILE *fp_in, FILE *fp_out);
+void compress_file(FILE *fp_in);
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
-		printf("Not enough arguments.\n");
+		fprintf(stderr, "Not enough arguments.\n");
 		exit(1);
 	}
 
-	FILE *fptr_w; 
-	char file_r[100], file_w[100];
-	// Last argv is target file
-	strcpy(file_w, argv[argc - 1]);
+	FILE *fptr_r;
 
-	fptr_w = fopen(file_w, "wb"); 
-	if (fptr_w == NULL) { 
-		printf("Cannot open file %s \n", file_w); 
-		exit(1); 
-	}
-
-	for (int i = 1; i < argc - 1; i++) {
-		// Open file for reading
-		FILE *fptr_r;
-		strcpy(file_r, argv[i]);
-		fptr_r = fopen(file_r, "r"); 
+	for (int i = 1; i < argc; i++) {
+		fptr_r = fopen(argv[i], "r"); 
 		if (fptr_r == NULL) { 
-			printf("Cannot open file %s \n", file_r); 
+			fprintf(stderr, "Cannot open file %s \n", argv[i]); 
 			exit(1); 
 		} else {
-			printf("compressing file %s\n", file_r);
-			compress_file(fptr_r, fptr_w);
+			compress_file(fptr_r);
 			fclose(fptr_r); 
 		}
 	}
-
-	fclose(fptr_w); 
 	return 0;
 }
 
-void compress_file(FILE *fp_in, FILE *fp_out) {
+void compress_file(FILE *fp_in) {
 	unsigned int count;
 	int ch = getc(fp_in);
 	int ch2 = ch;
@@ -52,8 +37,8 @@ void compress_file(FILE *fp_in, FILE *fp_out) {
 		for (count = 0; ch2 == ch && count < UINT_MAX; count++) {
 				ch2 = getc(fp_in);
 			}
-		fwrite(&count, 1, 4, fp_out);
-		fwrite(&ch, 1, 1, fp_out);
+		fwrite(&count, 1, 4, stdout);
+		fwrite(&ch, 1, 1, stdout);
 		ch = ch2;
 	}
 }
